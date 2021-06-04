934e61201c147f0efb1047d08a67ccf1c58d9eb5
rupalmalik3 <rupalmalik3@gmail.com>
2021-06-04 14:21:02 +0530

    fix : mailerlite link

--- dailykitwebsite/pages/sells/subscription_store.js
+++ dailykitwebsite/pages/sells/subscription_store.js
@@ -29,31 +29,7 @@ export default class SubscriptionStorepage extends PureComponent {
             defer
           ></script>
           {/* <!-- MailerLite Universal --> */}
-          {/* <script>
-      (function (m, a, i, l, e, r) {
-        m["MailerLiteObject"] = e;
-        function f() {
-          var c = { a: arguments, q: [] };
-          var r = this.push(c);
-          return "number" != typeof r ? r : f.bind(c.q);
-        }
-        f.q = f.q || [];
-        m[e] = m[e] || f.bind(f.q);
-        m[e].q = m[e].q || f.q;
-        r = a.createElement(i);
-        var _ = a.getElementsByTagName(i)[0];
-        r.async = 1;
-        r.src = l + "?v" + ~~(new Date().getTime() / 1000000);
-        _.parentNode.insertBefore(r, _);
-      })(
-        window,
-        document,
-        "script",
-        "https://static.mailerlite.com/js/universal.js",
-        "ml"
-      );
-      var ml_account = ml("accounts", "3161173", "g2o5g2c3i8", "load");
-    </script> */}
+          <script src="../components/mailerlite.js" defer></script>
           {/* <!-- End MailerLite Universal --> */}
           <script
             type="text/javascript"
