7e55f9e13f851c56a751034653cc5248359d7aa7
rupalmalik3 <rupalmalik3@gmail.com>
2021-06-07 11:51:25 +0530

    feat : added file for mailerlite form

---
+++ dailykitwebsite/pages/_document.js
@@ -0,0 +1,42 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class CustomDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {" "}
+          <script>
+            {/* (function (m, a, i, l, e, r) {
+        m["MailerLiteObject"] = e;
+        function f() {
+          var c = { a: arguments, q: [] };
+          var r = this.push(c);
+          return "number" != typeof r ? r : f.bind(c.q);
+        }
+        f.q = f.q || [];
+        m[e] = m[e] || f.bind(f.q);
+        m[e].q = m[e].q || f.q;
+        r = a.createElement(i);
+        var _ = a.getElementsByTagName(i)[0];
+        r.async = 1;
+        r.src = l + "?v" + ~~(new Date().getTime() / 1000000);
+        _.parentNode.insertBefore(r, _);
+      })(
+        window,
+        document,
+        "script",
+        "https://static.mailerlite.com/js/universal.js",
+        "ml"
+      );
+      var ml_account = ml("accounts", "3161173", "g2o5g2c3i8", "load"); */}
+          </script>
+        </Head>
+        <body>
+          <Main />
+        </body>
+        <NextScript />
+      </Html>
+    );
+  }
+}
