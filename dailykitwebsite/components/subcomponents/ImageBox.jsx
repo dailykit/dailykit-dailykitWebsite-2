a269a3400a4192812fc11d839fe3a0df7dd61525
rupalmalik3 <rupalmalik3@gmail.com>
2021-06-07 11:49:44 +0530

    fix: image tag in blogSection

--- dailykitwebsite/components/subcomponents/ImageBox.jsx
+++ dailykitwebsite/components/subcomponents/ImageBox.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-
 const ImageBox = (props) => {
   return (
     <div className="col">
       <div className="card imagebox-start-mealkit-brand">
-        <img src={props.rest.image} className="card-img-top" alt="..." />
+        {console.log(props.rest.image)}
+        <img
+          src={props.rest.image}
+          width="auto"
+          height="auto"
+          className="card-img-top"
+          alt="..."
+        />
         <div className="card-body">
           <h5 className="card-title">{props.rest.title}</h5>
           <small> {props.rest.small}</small>
