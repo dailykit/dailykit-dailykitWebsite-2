17f6579f273c3d2a51da4a9e2708f001199c0e8e
rupalmalik3 <rupalmalik3@gmail.com>
2021-06-10 15:20:34 +0530

    feat: added google tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ class MyDocument extends Document {
           <NextScript />
           <script defer src="/mailer.js" />
           <script defer src="/analytics.js" />
+          <script defer src="/googleTagManager" />
         </body>
       </Html>
     );
