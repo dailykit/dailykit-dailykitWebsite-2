398c83f2c7da20d0a2f4b6abbc2a8460be9a3048
rupalmalik3 <rupalmalik3@gmail.com>
2021-06-18 12:24:14 +0530

    fix: border of pricing column

--- components/PriceColumnLabel.js
+++ components/PriceColumnLabel.js
@@ -11,7 +11,7 @@ export default class PriceColumnLabel extends PureComponent {
               return <li key={list}> {list[0]}&nbsp;</li>;
             })}
           </ul>
-        </div>{" "}
+        </div>
         <div className="plan">
           <ul>
             <h4>Features</h4>
@@ -43,7 +43,7 @@ export default class PriceColumnLabel extends PureComponent {
               return <li key={list}> {list[0]}&nbsp;</li>;
             })}
           </ul>
-        </div>{" "}
+        </div>
         <div className="plan">
           <ul>
             <h4>Global Selliing</h4>
@@ -51,7 +51,7 @@ export default class PriceColumnLabel extends PureComponent {
               return <li key={list}> {list[0]}&nbsp;</li>;
             })}
           </ul>
-        </div>{" "}
+        </div>
         <div className="plan">
           <ul>
             <h4>Customization</h4>
@@ -59,7 +59,7 @@ export default class PriceColumnLabel extends PureComponent {
               return <li key={list}> {list[0]}&nbsp;</li>;
             })}
           </ul>
-        </div>{" "}
+        </div>
         <div className="plan">
           <ul>
             <h4>Editor App</h4>
@@ -67,7 +67,7 @@ export default class PriceColumnLabel extends PureComponent {
               return <li key={list}> {list[0]}&nbsp;</li>;
             })}
           </ul>
-        </div>{" "}
+        </div>
         <div className="plan">
           <ul>
             <h4>Content App</h4>
@@ -75,19 +75,18 @@ export default class PriceColumnLabel extends PureComponent {
               return <li key={list}> {list[0]}&nbsp;</li>;
             })}
           </ul>
-        </div>{" "}
+        </div>
         <div className="plan">
           <ul>
             {this.props.cartManagement.map((list) => {
               return (
                 <li style={{ borderTop: "none" }} key={list}>
-                  {" "}
                   Cart Management
                 </li>
               );
             })}
           </ul>
-        </div>{" "}
+        </div>
         <div className="plan">
           <ul>
             {this.props.insights.map((list) => {
@@ -98,9 +97,8 @@ export default class PriceColumnLabel extends PureComponent {
               );
             })}
           </ul>
-        </div>{" "}
+        </div>
         <div className="plan">
-          {" "}
           <ul>
             {this.props.support.map((list) => {
               return (
@@ -110,7 +108,7 @@ export default class PriceColumnLabel extends PureComponent {
               );
             })}
           </ul>
-        </div>{" "}
+        </div>
         <div className="plan">
           <ul>
             <h4>Payment Gateway Integration</h4>
